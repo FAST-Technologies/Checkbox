4ee81018068dcfb322852e85d8762f53058bc857
Vladimir Itmshikov <72025613+FAST-Technologies@users.noreply.github.com>
2023-08-30 00:19:06 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import "./styles.css";
+import React from "react";
+import Checkbox from "@mui/material/Checkbox";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+export default function App() {
+  const top100Films = [
+    { title: "The Shawshank Redemption", year: 1994 },
+    { title: "The Godfather", year: 1972 },
+    { title: "The Godfather: Part II", year: 1974 },
+    { title: "The Dark Knight", year: 2008 },
+    { title: "12 Angry Men", year: 1957 },
+    { title: "Schindler's List", year: 1993 },
+    { title: "Pulp Fiction", year: 1994 },
+    { title: "The Lord of the Rings: The Return of the King", year: 2003 },
+    { title: "The Good, the Bad and the Ugly", year: 1966 },
+    { title: "Fight Club", year: 1999 },
+    { title: "The Lord of the Rings: The Fellowship of the Ring", year: 2001 },
+    { title: "Star Wars: Episode V - The Empire Strikes Back", year: 1980 },
+    { title: "Forrest Gump", year: 1994 }
+  ];
+  return (
+    <Autocomplete
+      multiple
+      id="checkboxes-tags-demo"
+      options={top100Films}
+      disableCloseOnSelect
+      getOptionLabel={(option) => option.title}
+      renderOption={(option, { selected }) => (
+        <React.Fragment>
+          <Checkbox
+            icon={icon}
+            checkedIcon={checkedIcon}
+            style={{ marginRight: 8 }}
+            checked={selected}
+          />
+          {option.title}
+        </React.Fragment>
+      )}
+      style={{ width: 500 }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          variant="outlined"
+          label="Checkboxes"
+          placeholder="Favorites"
+        />
+      )}
+    />
+  );
+}
